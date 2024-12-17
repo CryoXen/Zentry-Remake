d171135ec06991dcb0a8d0b9f4c2f48e6e0ffea9
Christian <al222011226@gmail.com>
2024-12-17 17:03:24 -0600

    Integracion de NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 // import React from 'react'
 import About from "./components/About"
-
+import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
 const App = () => {
   return (
-    <main className="relative min-h-screen w-screen overflow-x-hidden">
+    <main className="relative min-h-screen w-screen overflow-x-hidden ">
+        <NavBar />
         <Hero />
-
         <About />
     </main>
   )
