75dbb248386634eba69be496ad66f20f563196a1
Christian <al222011226@gmail.com>
2024-12-18 18:45:03 -0600

    Tarjetas Interactivas

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import About from "./components/About"
 import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
+import Features from "./components/Features"
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden ">
         <NavBar />
         <Hero />
         <About />
+        <Features />
     </main>
   )
 }
