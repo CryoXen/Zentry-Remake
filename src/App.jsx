9672f4a6f79d9dad0877b0ab65551e9a2649d08f
Christian <al222011226@gmail.com>
2024-12-27 14:33:49 -0600

    Implementacion de Footer y Contacto

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import About from "./components/About"
 import NavBar from "./components/NavBar"
 import Hero from "./components/Hero"
 import Features from "./components/Features"
+import History from "./components/History"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden ">
@@ -10,6 +13,9 @@ const App = () => {
         <Hero />
         <About />
         <Features />
+        <History />
+        <Contact />
+        <Footer />
     </main>
   )
 }
